3f2f043ce76ca848e067625af2cdcb117b038487
Artem Gilmudinov <guliash92@gmail.com>
2015-07-19 20:17:25 +0300

    tricky constructor

---
+++ java/guliash3/Main.java
@@ -0,0 +1,21 @@
+//Is the code compilable?
+class A {
+
+    public A(String str) {
+
+    }
+}
+
+class B extends A {
+
+    public B(String str) {
+
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        B b = new B("hello");
+    }
+}
