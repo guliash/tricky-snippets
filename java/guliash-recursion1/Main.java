3e21a07a6dd8c16698326cce38a48bc8cd7825b6
Artem <gilmudinov_am@chelny.taximaxim.ru>
2015-11-30 09:40:05 +0300

    recursion example;

---
+++ java/guliash-recursion1/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+  static class A {
+    B b = new B();
+  }
+
+  static class B {
+    A a = new A();
+  }
+
+  public static void main(String[] args) {
+    A a = new A();
+  }
+}
